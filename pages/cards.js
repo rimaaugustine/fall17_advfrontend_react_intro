c124965db28948795c3239fae85fb5fb2122b956
Rima Faiqoh Augustine <augustine@campus.tu-berlin.de>
2017-10-02 20:23:37 +0200

    page card stash

--- pages/cards.js
+++ pages/cards.js
@@ -1,3 +1,48 @@
-/**
- * Created by macbook on 28.09.17.
- */
+import Head from "next/head"
+import Card from "../components/card.js"
+
+
+const cards = [
+    {
+        image:"static/dog.jpg", name:"rima", text: "aasadadad",
+    },
+    {
+        image:"static/dog.jpg", name:"rima", text: "asasasas",
+    },
+    {
+        image:"static/dog.jpg", name:"asasassa", text: "asas",
+    },
+    {
+        image:"static/dog.jpg", name:"timsdsd", text: "asda",
+    }
+
+]
+
+const Page = () => (
+
+    <div>
+        <Head>
+            <title>My seventh page!</title>
+            <link rel="stylesheet" href="static/main.css" />
+            <link rel="stylesheet" href="static/cards.css" />
+            <meta name="description" content="Let's make the web great again" />
+            <meta name="keywords" content="sample, react, demo, awesome" />
+        </Head>
+        <h1>Cards!</h1>
+
+        <div className="card-container">
+            {cards.map((item, index) => (<Card image= {item.image} name={item.name} text={item.text} key={index}/>)
+            )}
+
+
+        </div>
+    </div>
+
+
+
+
+)
+
+
+
+export default Page
